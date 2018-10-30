abf874601eadbb95494ed18f1dda9b871841a0d9
DimaChekashov <foxdima99@gmail.com>
2018-10-31 00:53:01 +0300

    end

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 var meteorContainer = document.getElementById('meteorites');
 var meteorRaining, drawMeteor, clearTrash;
 var s = 10;
+var scene = document.getElementById('scene');
+var scoreCount = 0;
+
+var score = function() {
+  console.log(scoreCount);
+  document.getElementById('score').innerText = scoreCount;
+}
 
 var Meteor = function(id) {
   this.id = id;
@@ -8,7 +15,7 @@ var Meteor = function(id) {
     var meteor = document.createElement('span');
     meteor.classList.add('meteor');
     meteor.setAttribute('countId', id);
-    meteor.setAttribute('top', 0);
+    meteor.setAttribute('top', -20);
     meteor.style.left = (Math.floor(Math.random() * (9 - 1) + 1) * 10) + '%';
     meteorContainer.appendChild(meteor);
   }
@@ -20,13 +27,25 @@ var spead = function() {
     meteors[i].style.top = parseInt(meteors[i].getAttribute('top')) + s + '%';
     meteors[i].setAttribute('top', parseInt(meteors[i].getAttribute('top')) + s);
   }
+  meteorsSave = document.getElementsByClassName('meteor-save');
+  for(i = 0; i < meteorsSave.length; i++) {
+    meteorsSave[i].style.top = parseInt(meteorsSave[i].getAttribute('top')) + 5 + '%';
+    meteorsSave[i].setAttribute('top', parseInt(meteorsSave[i].getAttribute('top')) + 5);
+  }
 }
 
 var clearMeteor = function() {
   meteors = document.getElementsByClassName('meteor');
   for(i = 0; i < meteors.length; i++) {
-    if(parseInt(meteors[i].getAttribute('top')) == 90) {
+    if(parseInt(meteors[i].getAttribute('top')) > 90) {
+      gameOver();
       meteorContainer.removeChild(meteors[i]);
+    }
+  }
+  meteorsSave = document.getElementsByClassName('meteor-save');
+  for(i = 0; i < meteorsSave.length; i++) {
+    if(parseInt(meteorsSave[i].getAttribute('top')) > 90) {
+      meteorContainer.removeChild(meteorsSave[i]);
     }
   }
 }
@@ -38,14 +57,31 @@ var createMeteor = function() {
   mCount++;
 }
 
+var destroyMeteor = function(e) {
+  if(e.target.classList == 'meteor') {
+    e.target.classList.remove('meteor');
+    e.target.classList.add('meteor-save');
+    scoreCount++;
+    score();
+  }
+}
+
 var initalGame = function() {
   meteorRaining = setInterval(createMeteor, 1000);
   drawMeteor = setInterval(spead, 1000);
   clearTrash = setInterval(clearMeteor, 1000);
+  scene.addEventListener('click', destroyMeteor);
 }
 
 var gameOver = function() {
   clearInterval(meteorRaining);
+  clearInterval(drawMeteor);
+  clearInterval(clearTrash);
+  document.getElementById('over-screen').style.display = 'flex';
+  /* meteors = document.getElementsByClassName('meteor');
+  for(i = 0; i < meteors.length; i++) {
+    meteorContainer.removeChild(meteors[i]);
+  } */
 }
 
 var gameStart = function() {
